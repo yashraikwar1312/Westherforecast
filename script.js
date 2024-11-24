6af12e1ba607de65c372410d3eda5a890aa1519e
yashraikwar1312 <cse24_yashraikwar@ggct.co.in>
2024-11-24 16:35:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('getWeatherBtn').addEventListener('click', function() {
     const city = document.getElementById('cityInput').value;
-    const apiKey = 'YOUR_API_KEY'; // Replace with your OpenWeatherMap API key
+    const apiKey = 'f9554c629673f64e2d4c1fbdaaf20ced'; // Replace with your OpenWeatherMap API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(apiUrl)
